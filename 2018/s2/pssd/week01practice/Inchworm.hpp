775ec8c804e9860ed616f973fb1a02b3a108a056
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-07-22 12:03:41 +0000

    1--1

---
+++ 2018/s2/pssd/week01practice/Inchworm.hpp
@@ -0,0 +1,38 @@
+class Inchworm {
+    
+    public:
+        Inchworm(){}
+        int lunchtime(int branch, int rest, int leaf){
+            int leafEaten = 0;
+            bool allValid = true;
+            if (!( 0 < branch < 1000000) ){
+                cout << "Invalid branch number\n";
+                allValid = false;
+            }
+            if (!( 0 < rest< 1000)){
+                cout << "Invalid rest number\n";
+                allValid = false;
+            }
+            if (!( 0 < leaf< 1000)){
+                cout << "Invalid leaf number\n";
+                allValid = false;
+            }
+            
+            if (allValid == true){
+                    
+                for (int i = 0; i < (branch / leaf) + 1; i++){
+                    if ( (i * leaf ) % rest == 0){
+                        leafEaten +=1;
+                    }
+                    else{
+                        leafEaten +=0;
+                    }
+                }
+                return leafEaten;
+            }
+            else {
+                return -1;
+            }
+            
+        }
+};
