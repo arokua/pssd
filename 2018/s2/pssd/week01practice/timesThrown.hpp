a9949941f027430db7960f92d78d01b566ff721a
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-07-24 22:38:00 +0000

    1-2 one

---
+++ 2018/s2/pssd/week01practice/timesThrown.hpp
@@ -0,0 +1,45 @@
+#include <iostream>
+
+using namespace std;
+
+class ThrowTheBall {
+    public:
+    int TimesThrown(int N, int M, int L){
+
+        int currentPlayer = 1;
+        int times = 0;
+        int BHBT[N][1] ; //ballHoldernBallTimes
+        BHBT[0][0] = 1;
+        // 
+        for (int i = 1;i < N; i++){
+            BHBT[i][0] = 0;
+        }
+        while (true){
+            
+            if (BHBT[currentPlayer - 1][0] == M){
+                break;
+            }
+            //Decicding player holding the ball
+            times += 1;
+            if (BHBT[currentPlayer - 1][0] % 2 == 0){
+                if ((currentPlayer  + L) % N != 0){
+                    currentPlayer = (currentPlayer + L) % N;
+                }else {
+                    currentPlayer = N;
+                }
+            }else if (BHBT[currentPlayer - 1][0] % 2 != 0){
+                if ((currentPlayer  - L) % N != 0){
+                    currentPlayer = (currentPlayer - L + N) % N;
+                }else {
+                    currentPlayer = N;
+                }
+            }
+            
+            //Increase the times holding it
+            BHBT[currentPlayer-1][0] += 1;
+            
+        }
+        
+        return times;
+    }
+};
