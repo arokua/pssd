8882938315f1cd9e0f8cf399f9fb78153c113578
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-07-27 12:53:56 +0000

    x

---
+++ week01practice/AzimuthMonitoring.hpp
@@ -0,0 +1,38 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+class AzimuthMonitoring {
+    public:
+    int getAzimuth(vector<string> instructions){
+        int angle = 0;
+        int x = 0;
+        for (int i = 0; i < instructions.size(); i ++){
+            if (instructions[i] == "RIGHT"){
+                angle += 90;
+            }else if(instructions[i] == "LEFT"){
+                angle -= 90; 
+                if (angle < 0){
+                    angle += 360;
+                }
+            }else if(instructions[i] == "TURN AROUND"){
+                angle += 180;
+            }else if (instructions[i] == "HALT"){
+                break;
+            }else{
+                if (instructions[i].find("RIGHT") == 0){
+                    
+                    instructions[i].replace(0,6,"");
+                    angle += stoi(instructions[i]);    
+                    
+                }else if (instructions[i].find("LEFT") == 0){
+                    instructions[i].replace(0,5,"");
+                    angle += stoi(instructions[i]);              
+                }
+            }
+        }
+        return angle%360;
+    }
+};
