ca07ab3382b4fc7acac10724b20fc248148a234f
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-01 23:28:27 +0000

    1-2

--- week02practice/ChangingString.hpp
+++ week02practice/ChangingString.hpp
@@ -4,4 +4,4 @@
 
 using namespace std;
 
-class ChangingString { public: int distance(string A, string B, int K) { int d = 0; for (int i =0; i < K;i++){ d+= abs(A.at(i) - B.at(i)); } return d; } };
+class ChangingString { public: int distance(string A, string B, int K) { int d = 0; for (int i =0; i < K;i++){ d+=A.at(i) - B.at(i); } return d; } };
