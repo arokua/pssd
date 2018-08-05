ea0ba47c462607aa55bac1a5a7285862c707cce9
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-05 06:24:09 +0000

    BR cards

---
+++ week02practice/BlackandRed.hpp
@@ -0,0 +1,53 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class BlackandRed {
+    public:
+    int cut(string deck){
+        
+        
+        int minVal = deck.length();
+        
+        bool correctCut =false;
+        
+        cout << "Length "<< deck.length()<<endl;
+        for (int i = 0; i < deck.length(); i++){
+            string cutteddeck = deck.substr(i, deck.length() - i) + deck.substr(0, i);
+            int nRed = 0;
+            int nBlack = 0;
+            for (int j = 0; j < cutteddeck.length(); j ++){
+                
+                
+                if (cutteddeck[j] == 'R'){
+                        nRed += 1;
+                }else if (cutteddeck[j] == 'B'){
+                    nBlack += 1;
+                }
+                
+                if (nBlack < nRed){
+                    cout << i << " is fail!" << endl;
+                    cout << i << " Ratio: R::" << nRed<< " B: "<<nBlack<<  endl;
+                    break;
+                }
+                else if (!(nBlack < nRed)){
+                    if (j == cutteddeck.length() - 1){
+                        
+                        if (minVal > i){
+                            minVal = i;
+                        }
+                    }
+                }
+                
+                    
+                
+            }
+            
+            
+        }
+            
+        return minVal;
+        
+    }
+};
