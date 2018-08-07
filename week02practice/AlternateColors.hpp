bbefd0226a329fa35f38b0f56205e449e403df43
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-07 11:49:07 +0000

    ss

--- week02practice/AlternateColors.hpp
+++ week02practice/AlternateColors.hpp
@@ -9,28 +9,44 @@ class AlternateColors{
         long sumNow = 0;
         int turn = 1;
         while (sumNow < k){
-        
+            cout << turn << " ";
             if ( turn == 1 && r > 0){
                 r -= 1;
                 sumNow+=1;
                 if (sumNow!= k){
                     turn += 1;
+                    if (g > 0){
+                        turn +=1;
+                    }else if (b > 0){ 
+                        turn += 2;
+                    }else {
+                        turn = 1;
+                    }
                 }
             }
             else if ( turn == 2 && g > 0){
                 g -= 1;
                 sumNow+=1;
-                if (sumNow != k){
-                    turn +=1;
-                }
+                if (sumNow != k ){
+                    if (b > 0){
+                        turn +=1;
+                    }else if (r > 0){ 
+                        turn -= 2;
+                    }else {
+                        turn = 2;
+                    }                }
             }
             
             else if ( turn == 3 && b > 0){
                 r -= 1;
                 sumNow+=1;
-                if (sumNow != k){
-                    turn -= 2;
-                }      
+                    if (g > 0){
+                        turn -= 1;
+                    }else if (r > 0){ 
+                        turn -= 2;
+                    }else {
+                        turn = 3;
+                    }     
             }
         }
         
