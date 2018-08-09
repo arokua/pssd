f8fbf9ab75bfe7e2e4d51ae3ea1413c037953271
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-09 10:38:21 +0000

    s

--- 2018/s2/pssd/week02practice/AlternateColors.hpp
+++ 2018/s2/pssd/week02practice/AlternateColors.hpp
@@ -4,58 +4,69 @@
 using namespace std;
 
 class AlternateColors{
+    private:
+        long sum = 0;
+        int t = 1;
     public:
+    void storeSum(long s){
+        sum = s;
+    }
+    long getSum(){
+        return sum;
+    }
+    
+    void storeTurn(int t1){
+        t = t1;
+    }
+    int getTurn(){
+        return t;
+    }
+    
     string getColor(long r, long g, long b, long k){
-        long sumNow = 0;
-        int turn = 1;
-        while (sumNow < k){
-            
-            if ( turn == 1 && r > 0){
+        long sumNow = getSum();
+        int turn = getTurn();
+        
+        string colors[3] = {"RED","GREEN","BLUE"};
+        
+        if (sumNow == k){
+            return colors[turn - 1];
+        }
+        else if (sumNow < k){
+            storeSum(sumNow += 1);
+            if (r > 0 && turn == 1){
                 r -= 1;
-                sumNow+=1;
-                if (sumNow!= k){
-                    if (g > 0){
-                        turn +=1;
-                    }else if (b > 0 && g < 1){
-                        
-                        turn += 2;
-                    }else if (r > 0 && g < 1){
-                        turn = 1;
-                    }
+                if ( g > 0){
+                    turn = 2;
+                }else if (b > 0){
+                    turn = 3;
+                }
+                else  {
+                    turn = 1;
+                }
+            }else if (g > 0 && turn == 2){
+                g -= 1;
+                if ( r > 0){
+                    turn = 1;
+                }else if (b > 0){
+                    turn = 3;
+                }
+                else  {
+                    turn = 2;
+                }
+            }else if (b > 0 && turn == 3){
+                b -= 1;
+                if ( r > 0){
+                    turn = 1;
+                }else if (g > 0){
+                    turn = 2;
+                }
+                else  {
+                    turn = 3;
                 }
             }
-            else if ( turn == 2 && g > 0){
-                g -= 1;
-                sumNow+=1;
-                if (sumNow != k ){
-                    if (b > 0){
-                        turn +=1;
-                    }else if (r > 0 && b < 1){ 
-                        turn -= 1;
-                    }else {
-                        turn = 2;
-                    }                }
-            }
-            
-            else if ( turn == 3 && b > 0){
-                r -= 1;
-                sumNow+=1;
-                    if (g > 0 && r < 0){
-                        turn -= 1;
-                    }else if (r > 0 ){ 
-                        turn -= 2;
-                    }else {
-                        turn = 3;
-                    }     
-            }
-        }
-        
-        if (turn == 1){
-            return "RED";
-        }else if (turn == 2){
-            return "GREEN";
-        }else if (turn == 3){
-            return "BLUE";
+            storeTurn(turn);
+            return getColor(r,g,b,k);
         }
     }
+        
 };
