dc0817676af8f9a2d22792671428cebbcd0c5374
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-21 12:50:31 +0000

    ffs

---
+++ week03practice/FewestFactors.hpp
@@ -0,0 +1,61 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class FewestFactors{
+    private:
+    int totalFactor(int a){
+        int tot = 0;
+        for (int i = 1;i < a + 1; i++){
+            if (a%i == 0){
+                tot++;
+            }
+        }
+        return tot;
+    }
+    
+    int Intpower (int base, int expo) {
+        int ans = 1;
+        if (expo == 0){
+            return 1;
+        }
+        else if ( expo > 0){
+            for (int i = 0; i < expo; i ++){
+                ans *= base;
+            }
+            return ans;
+        }
+        else if (expo < 0) {
+            return 0;
+        }
+        else {
+            return 0;
+        }
+    }
+    
+    public:
+    int number(vector<int> digits){
+        
+        int iS = 0;
+        int ret ;
+        
+        for (int i = 0; i < digits.size();i ++){
+            iS += Intpower(10,digits.size() - i - 1) * digits[i];
+        }
+        
+        ret = iS;
+        while (next_permutation(digits.begin(),digits.end())){
+            iS = 0;
+            for (int i = 0; i < digits.size();i ++){
+            iS += Intpower(10,digits.size() - i - 1) * digits[i];
+            }
+        
+            if (totalFactor(ret)> totalFactor(iS)){
+                ret = iS;
+            }
+        }
+        
+        return ret;
+    }
+};
