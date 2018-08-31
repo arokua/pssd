f018d9dda3a03fa5e93439c0ebbef485fbe7b190
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-08-31 01:35:18 +0000

    nothing

--- week05practice/main.cpp
+++ week05practice/main.cpp
@@ -1,9 +1,26 @@
 #include <iostream>
-#include "RugSizes.hpp"
+#include <vector>
+#include "SimpleDuplicateRemover.hpp"
 
 using namespace std;
 
 int main(){
-	RugSizes A;
-	cout<< A.rugCount(30)<< "\n";
+	SimpleDuplicateRemover A;
+
+	vector<int> C;
+
+	C.push_back(1);
+	C.push_back(5);
+	C.push_back(5);
+	C.push_back(1);
+	C.push_back(6);
+	C.push_back(6);
+	C.push_back(1);
+
+
+
+	for (int i =0; i < A.process(C).size(); i++){
+		cout << A.process(C)[i] << " ";
+	}
+	cout <<endl;
 }
