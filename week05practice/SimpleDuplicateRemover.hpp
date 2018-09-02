09730526d8ddd3d880fd23aed64f553de0ac4e8a
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-09-02 00:36:38 +0000

    reversal and parameters notice

--- week05practice/SimpleDuplicateRemover.hpp
+++ week05practice/SimpleDuplicateRemover.hpp
@@ -5,7 +5,7 @@ using namespace std;
 
 class SimpleDuplicateRemover{
 	public:
-		bool vecFind(vector<int> V, int n){
+		/*bool vecFind(vector<int> V, int n){
 			sort(V.begin(), V.end());
 			int counter = 0;
 			for (int i = 0; i < V.size(); i++){
@@ -19,21 +19,22 @@ class SimpleDuplicateRemover{
 					}
 				}
 			}
-		}
+		}*/
 
 		vector<int> process(vector<int> sequence){
 			vector<int>  ret;
+			vector<int>  ter;
 			ret.push_back(0);
 
-			for (int i = sequence.size()-1; i>-1;i--){
-				if (vecFind(ret,sequence[i]) == false){
-					cout << sequence[i]<<endl;
+			for (int i = sequence.size() - 1; i > -1; i--){
+				if (!any_of(ret.begin(), ret.end(), [=](int t){return t == sequence[i];})){
 					ret.push_back(sequence[i]);
-				}else if (vecFind(ret,sequence[i]) == true) {
-					continue;
+
 				}
 			}
+			
 			ret.erase(ret.begin() + 0);
+			reverse(ret.begin(),ret.end());
 			return ret;
 		}
 };
