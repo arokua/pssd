7a89f7f2289507e714153894597af5a9d5b11232
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-09-11 13:32:10 +0000

    howuitgo nope

---
+++ 2018/s2/pssd/week06practice/JumpyNum.hpp
@@ -0,0 +1,44 @@
+// Example program
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+class JumpyNum{
+    public:
+    int isJumpy(int n){
+        string s = to_string(n);
+        if (int(s.length()) == 1){
+            return true;
+        }else{
+            int count = 0;
+            for (int i = 0; i < int(s.length()) - 1;i++){
+                if (s[i] - s[i+1] > 1){
+                    
+                    count ++;
+                }else if (s[i +1] - s[i] > 1){
+                    
+                    count++;
+                }
+            }
+            
+            if (count == int(s.length()) -1){
+                return true;
+            }else {
+                return false;
+            }
+        
+        }
+    }
+    
+    
+    int howMany(int low, int high){
+        long counter = 0;
+        for (int i = low; i < high + 1; i++){
+            if (isJumpy(i)){
+                counter += 1;
+            }
+        }
+        return counter;
+    }
+};
