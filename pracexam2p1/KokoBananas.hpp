864865b389a1755ea0fc2a6ca1fb33787c529db8
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-09-14 02:06:58 +0000

    nothing

--- pracexam2p1/KokoBananas.hpp
+++ pracexam2p1/KokoBananas.hpp
@@ -1,5 +1,6 @@
 #include <vector>
 #include <algorithm>
+#include <iostream>
 
 using namespace std;
 
@@ -20,6 +21,23 @@ public:
 
 		return re;
 	}
+
+	vector<int> minVec(vector<int> v){
+		vector<int> re; // pos, val
+		re.reserve(2);
+		int c = 0;
+		for (int i = 0; i < v.size(); i++) {
+			c = min(c,v[i]);
+			if (v[i] == c){
+				re[0] = i;
+				re[1] = c;
+			}
+
+		}
+
+		return re;
+	}
+
 	bool vecEqual(vector<int> v){
 		int c = v[0];
 		for (int i = 1; i < v.size(); i++){
@@ -40,9 +58,28 @@ public:
 			return true;
 		}
 	}
+
+	vector<int> findDuplicatePos(vector<int> a, int d2Pos){
+		vector<int> z;
+		int known = a[d2Pos];
+		for (int i = 0; i < a.size(); i++){
+			if (a[i] == known && i != d2Pos){
+				z.push_back(i);
+			}
+		}
+
+		return z;
+	}	
+
 	int n ;
 	int lastHand(vector<int>hands, int K){
-		n = maxVec(hands)[0];
+
+		if (!hasDuplicate(hands, maxVec(hands)[1]) ){
+			n = maxVec(hands)[0];
+		}else {
+
+			n= minVec(findDuplicatePos(hands,maxVec(hands)[0]) )[0] ;
+		}
 		if (K == 0){
 			return n;
 
@@ -51,12 +88,9 @@ public:
 
 
 		else {
-			if (!hasDuplicate( hands, maxVec(hands)[1] ) ){
-				hands[maxVec(hands)[0]] -= 1;
-				return lastHand(hands, K -1);
-			}
-
-			
+			hands[maxVec(hands)[0]] -= 1;
+			return lastHand(hands, K -1);			
 		}
+		cout << n;
 	}
 };
