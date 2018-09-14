8035fadb13141e11dc744c8860f403ce3ae041cf
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-09-14 02:11:07 +0000

    nothing

--- pracexam2p1/main.cpp
+++ pracexam2p1/main.cpp
@@ -7,9 +7,9 @@ using namespace std;
 int main() {
 	KokoBananas K;
 	vector<int> a;
-	a.push_back(4);
-	a.push_back(9);
 	a.push_back(5);
+	a.push_back(8);
+	a.push_back(0);
 	
-	cout <<K.lastHand(a,18)<<endl;
+	cout <<K.lastHand(a,4)<<endl;
 }
