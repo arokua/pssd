d9d0dcb3be35b95a12d7653475c07a2f65fad2e7
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-09-17 00:26:18 +0000

    fix some thing

--- week07practice/PaternityTest.hpp
+++ week07practice/PaternityTest.hpp
@@ -1,6 +1,6 @@
-#include <iostream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 using namespace std;
 class PaternityTest{
@@ -8,42 +8,40 @@ class PaternityTest{
     vector<int> possibleFathers(string child, string mother, vector<string> men){
         vector<char> d;
         vector<int> n;
-        int c = 0;
+        bool c = false;
         int matchT = 0;
         vector<int> posG;
         
-        for (int i = 0; i < 4; i++){
+        for (int i = 0; i < child.length(); i++){
             if (child[i] != mother[i]){
                 d.push_back(child[i]);
                 n.push_back(i);
             }
         }
-
+        if (mother == child){
+            c = true;
+        }
         
         for (int i = 0; i < int(men.size()); i++){
             string dadbe = men[i];
-            for (int j = 0; j < 4; j++){
-                if (c == 0){
-                    if (j == n[c] && dadbe[j] == d[c]){
+            for (int j = 0; j < child.length(); j++){
+                if (!c){
+                    if (dadbe[j] == child[j] && any_of(n.begin(),n.end(),[=](int x){return x == j;}) ){
                         matchT++;
-                        c++;
                     }
-                    
-                }else if ( c >= 1){
-                    if (j == n[c] && dadbe[j] == d[c]){
+                }
+                else{
+                    if (dadbe[j] == child[j] ) {
                         matchT++;
-                        c++;
                     }
-                    
                 }
                 
             }
             
-            if (matchT >=1){
+            if (matchT >= int(child.length()) / 2){
                 posG.push_back(i);
             }
             matchT = 0;
-            c = 0;
         }
         
         return posG;
