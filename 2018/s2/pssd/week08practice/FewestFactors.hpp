08045b0cf823d81ae7c63e142d59ae892b54a801
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-05 02:24:39 +0000

    mmm

---
+++ 2018/s2/pssd/week08practice/FewestFactors.hpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+
+using namespace std;
+
+class FewestFactors {
+public:
+	int baseNTo(int n, int power){
+		int ans = 1;
+		if (power == 0){
+			return 1;
+		}
+		for (int i = 0; i < power; i++){
+			ans *= n;
+		}
+		return n;
+	}
+
+	int makeNums(vector<int> digits){
+		int num = 0;
+		for (int i = digits.size() - 1; i > -1; i--){
+			num += digits[i]* baseNTo(10, digits.size() - i - 1);
+		}
+		return num;
+	}
+
+	bool checkPrime(int n){
+		for (int i = 2; i < (n / 2) + 1; i++){
+			if (n % i == 0){
+				return false;
+			}
+		}
+		return true;
+	}
+
+	int nFactor(int n){
+		if (checkPrime(n)){
+			return 2;
+		}
+		int re = 2;
+		for (int i = 2; i < n / 2; i ++){
+			if (n % i == 0){
+				re++;
+			}
+		}
+		return re;
+	}
+
+	int number(vector<int> digits){
+		sort(digits.begin(), digits.end());
+		int currentNum = makeNums(digits);
+		int currentFac = nFactor(currentNum);
+		
+		int compare = 0;
+
+		while (next_permutation)
+		
+	}
+};
