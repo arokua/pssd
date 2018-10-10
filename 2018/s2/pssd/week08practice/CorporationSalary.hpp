d6595b80ac291474089971c66303bf865db23efc
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-10 12:46:44 +0000

    CSCcs

---
+++ 2018/s2/pssd/week08practice/CorporationSalary.hpp
@@ -0,0 +1,88 @@
+#include <vector>
+
+#include <string>
+
+
+using namespace std;
+
+
+
+/*class Dict{
+    string name;
+    
+    public:
+    Dict(int n){
+        if (n == 0){
+            vector<string> name;
+        }
+    }
+    void addElement(string element){
+        
+    }
+};
+
+
+
+class makeClass{
+   public:
+   void makeDict(string name, int type){
+       // 0: string, 1 : int, 2: double
+       Dict name;
+   }
+};*/
+
+
+
+class CorporationSalary{
+
+    public:
+
+    int countEmployees( string staff){
+
+        int employed = 0;
+
+        for (int i = 0; i < int(staff.length()); i++){
+
+            if (staff[i] == 'Y'){
+
+                employed++;
+
+            }
+
+        }
+
+        return employed;
+
+    }
+
+
+
+    long totalSalary(vector<string> relations){
+
+        vector<int> employed;
+
+        long re = 0;
+
+        employed.reserve(relations.size());
+
+        for (int i = 0; i < relations.size(); i++){
+
+            if (countEmployees(relations[i]) > 0 ){
+
+                re += countEmployees(relations[i]);
+
+            }else {
+
+                re += 1;
+
+            }
+
+        }
+
+        
+
+	return re;
+
+    }
+
+};
