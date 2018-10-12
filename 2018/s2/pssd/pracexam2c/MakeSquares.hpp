97bf9bf4257673a674818289aa04ce41518fc69e
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-12 01:20:24 +0000

    nothing2

--- 2018/s2/pssd/pracexam2c/MakeSquares.hpp
+++ 2018/s2/pssd/pracexam2c/MakeSquares.hpp
@@ -1,17 +1,24 @@
 #include <algorithm>
+#include <cmath>
 
 using namespace std;
 
 class MakeSquares{
 public:
 	int maxCount(int N){
-		if (N == 4){
-			return 1;
+		double dN = N * 1.0;
+		if (sqrt(dN) == int(sqrt(dN))) {
+			//there is a patern wwith square roots, too late to implement it, have to edo with the nextr number after a int square roor have the same squares, and the one after that is +1
 		}
-		if (N % 2 == 1){
-			return maxCount(N - 1);
-		}else {
-			return (1 + maxCount(N - 2)) * 1;
+		if (N < 6){
+			return 1;
+		}else if (N < 8){
+			return 2;
+		}else if (N == 8){
+			return 3;
+		}
+		else if (N == 9 || N == 10){
+			return 5;
 		}
 	}
 };;
