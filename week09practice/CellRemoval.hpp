4b7c5f5472725944049880b6f4c70c6bb6a1d217
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-16 09:57:30 +0000

    something

---
+++ week09practice/CellRemoval.hpp
@@ -0,0 +1,51 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class CellRemoval{
+    public:
+    vector<int> deleteCell(vector<int> cells, int unlucky){
+        vector<int> newZ;
+        vector<int> grandChildren;
+        for (int i = 0; i < int(cells.size()); i++){
+            if (i != unlucky && cells[i] != unlucky && cells[i] != unlucky ){
+                newZ.push_back(cells[i]);
+            }
+            
+            if (cells[i] == unlucky){
+                grandChildren.push_back(i);
+            }                
+        }
+        
+        if ( grandChildren.size() == 1){
+            newZ = deleteCell(newZ, grandChildren[0]);
+        }
+        else if (grandChildren.size() == 2){
+            int a = grandChildren[0];
+            int b = grandChildren[1];
+            newZ = deleteCell(newZ,a);
+            newZ = deleteCell(newZ,b);
+        }
+        if (grandChildren.size() == 0){
+            return newZ;
+        }   
+    }
+    
+    int cellsLeft(vector<int> c, int d){
+        if (c[d] == -1){
+            return 0;
+        }else {
+            vector<int> z = deleteCell(c, d);
+            sort(z.begin(),z.end());
+            int l = 0;
+            for (int i = 0; i < z.size() - 1; i++){
+                if (z[i] != z[i + 1]){
+                    l++;
+                }
+            }
+            return l;
+        }
+    }
+};
