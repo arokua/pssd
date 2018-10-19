265ed3f59731a0527b71dd2f6958714efcd1cdae
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-19 01:05:59 +0000

    1234nope

---
+++ 2018/s2/pssd/week10practice/RockStar.hpp
@@ -0,0 +1,40 @@
+#include <algorithm>
+
+using namespace std;
+
+class RockStar{
+	int s = 0;
+public:
+	int getNumSong(int ff, int fs, int sf, int ss){
+		if (ff == 0 && fs == 0){
+			if (ss == 0 && sf > 0){
+				int a = s;
+				s = 0;
+				return a + 1;
+			}else if (ss > 0 ){
+				int a = s;
+				s = 0;
+				if (sf > 0){
+					
+					return a + ss + 1;
+				}else {
+					return a + ss;
+				}
+			}
+		}
+
+		else if (ff > 0 && fs == 0 ){
+			int a = s;
+			s = 0;
+			return a + ff;
+		}else if (fs > 0 ){
+			if (sf == 0 && ss == 0 ){
+				int a = s;
+				s = 0;
+				return a + 1;
+			}
+			s++;
+			return getNumSong(ff,fs-1,sf,ss);
+		}
+	}
+};
