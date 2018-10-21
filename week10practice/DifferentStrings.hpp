6ff22011d307d563ddb8303e2cc1f904890130ca
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-21 23:00:08 +0000

    DS

---
+++ week10practice/DifferentStrings.hpp
@@ -0,0 +1,32 @@
+#include <string>
+
+using namespace std;
+
+class DifferentStrings {
+    public:
+    int Diff(string a, string b){
+        int c = 0;
+        for (int i = 0; i < int(a.length()); i ++){
+            if (a[i] != b[i]){
+                c++;
+            }
+        }
+        return c;
+    }
+    
+    int minimize(string A, string B){
+        if (A.length() == B.length()){
+            return Diff(A,B);
+        }
+        int re = B.length();    
+        for (int i = 0; i < B.length(); i++){
+            if ( i > (A.length() - 1)){
+                break;
+            }
+            string s = B.substr(i, A.length());
+            re = min(re, Diff(A, s));
+        }
+        
+        return re;
+    }
+};
