eb4a6bcde7e455599c6c5663e24a8461ab14e1c0
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-22 11:57:58 +0000

    BB

---
+++ week10practice/BigBurger.hpp
@@ -0,0 +1,57 @@
+#include <algorithm>
+
+#include <vector>
+
+
+
+using namespace std;
+
+
+
+class BigBurger{
+
+    public:
+
+    int maxWait(vector<int> a, vector<int> s){
+
+        
+ if (a.size() == 1){
+            return 0;
+
+        }
+
+
+        int tiMe = 0;
+
+        tiMe = a[0] + s[0];
+
+        int wait ;
+
+        
+if (tiMe >= a[1]){
+
+            wait = tiMe - a[1];
+
+            tiMe += s[1];
+
+        }
+        else {
+            tiMe = a[1];
+            wait = 0;
+        }
+        int i = 2;
+        while ( i <a.size() ){
+            if (tiMe >= a[i]){
+                
+                wait = max(tiMe - a[i], wait);
+                tiMe += s[i];
+            }
+            else {
+                wait = 0;
+                tiMe = a[i];
+            }
+            i++; 
+        }
+        return wait;
+    }
+};
