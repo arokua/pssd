e3d645412c9fd07722e6667818530f1b8bebffd0
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2018-10-24 05:23:14 +0000

    JN

--- week10practice/JumpyNum.hpp
+++ week10practice/JumpyNum.hpp
@@ -1,42 +1,56 @@
+#include <iostream>
 #include <string>
 
 using namespace std;
 
+int IntPow(int base, int n){
+    if (n == 0){
+        return 1;
+    }
+    return base * IntPow(base, n -1);
+}
+
 class JumpyNum{
     public:
-    int isJumpy(int n){
-        string s = to_string(n);
-        if (int(s.length()) == 1){
-            return true;
-        }else{
-            int count = 0;
-            for (int i = 0; i < int(s.length()) - 1;i++){
-                if (s[i] - s[i+1] > 1){
-                    
-                    count ++;
-                }else if (s[i +1] - s[i] > 1){
-                    
-                    count++;
-                }
-            }
-            
-            if (count == int(s.length()) -1){
-                return true;
-            }else {
-                return false;
-            }
-        
-        }
-    }
-    
+
     
     int howMany(int low, int high){
-        long counter = 0;
-        for (int i = low; i < high + 1; i++){
-            if (isJumpy(i)){
-                counter += 1;
+        int counter = 0;
+        
+        if (low == high){
+            if (low % IntPow(10, int(to_string(low).length() - 1) ) == 0){
+                return 0;
             }
         }
+        for (int i = low; i < high + 1; i++){
+            string s = to_string(i);
+            int n = 0;
+            int len = s.length();
+            if (i < 10){
+
+                counter++;
+
+            }if (i == 10){
+                counter+= 0;
+            }if (19 < i < 91 && i % 10 == 0 ){
+                counter++;
+            }if (i > 99 && i % IntPow(10, int(len - 1) ) == 0){
+                counter += 0;
+            }
+            else {
+                for (int j = 0; j < len; j++ ){
+                    int c = int(s[j]) - int(s[j + 1]) ;
+                    if (c != 0 and c != -1 and c != 1 ){
+                        n++;
+                    } 
+                }
+                
+                if (n == len  - 1){
+                    counter++;
+                }
+            }           
+        }
+        
         return counter;
     }
 };
