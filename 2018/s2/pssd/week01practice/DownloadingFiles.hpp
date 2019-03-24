16e38d6ab413def12009cc466a5c6bc54a2fa4bc
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2019-03-24 21:42:11 +0000

    spacing

--- 2018/s2/pssd/week01practice/DownloadingFiles.hpp
+++ 2018/s2/pssd/week01practice/DownloadingFiles.hpp
@@ -9,29 +9,20 @@ class DownloadingFiles{
     public:
     double actualTime(vector<string> tasks){
         double toRe = 0;
-        
-        
-        
-        
-        vector<pair <double,double>> SpdnT; // Spedd and time in double
+        vector<pair <double,double>> SpdnT; // Speed and time in double
         
         // Getting two seperate values;
-        
         for (int i = 0; i < tasks.size(); i++){
             for (int j = 0; j < tasks[i].length();j++){
                 if (tasks[i][j] == ' '){
-                    
                     SpdnT.push_back(make_pair(stod(tasks[i].substr(0,j)), stod(tasks[i].substr(j+1, tasks[i].length() - 1))));
-                    
                     break;
                 }
             }
         }
         
         //Start looping over arrays elements to sort and calculate
-        
         for (int n = 0; n < SpdnT.size(); n ++){
-            
             double free = 0;// Speed left over
             double SmallestTime = 10000;
             int aPos = 0;
@@ -51,18 +42,13 @@ class DownloadingFiles{
                     SpdnT[b].second = 0;
                     free += SpdnT[b].first;
                 }
-                
                 if ( SpdnT[b].second >0){
                     aPos = b;
-                    
                 }
-        
                 SpdnT[aPos].second = SpdnT[aPos].first*SpdnT[aPos].second/(SpdnT[aPos].first + free);
                 SpdnT[aPos].first += free;
             }
         }
-                
-        
         return toRe;
     }
 };
