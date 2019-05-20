4216d32cf03655a0768bd66c3898d6262a20e9ce
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2019-05-20 08:07:55 +0000

    slightly better

--- 2018/s2/pssd/week07practice/classPrefixFreeSet.hpp
+++ 2018/s2/pssd/week07practice/classPrefixFreeSet.hpp
@@ -4,26 +4,33 @@
 #include <algorithm>
 
 using namespace std;
-class PrefixFreeSet {
-public:
-	bool isPrefix(string a, string b){
+
+bool isPrefix(string a, string b){
 		if (a.length() > b.length()) return isPrefix(b, a);
 		if (b.substr(0, ( int(a.length() )) ) == a ) return true;
 		return false;
 	}
-	int maxElements(vector<string> words){
-		//Sort the list 
-		sort(words.begin(), words.end());
-		int re = 1;
-		int temp = 1;
-		for (int i = 0; i < int( words.size() ) - 1; i++){
-			if (!isPrefix(words[i], words[i + 1])){
-				temp++;
-			}else {
-				re = max(re, temp);
-				temp = 1;
-			}
-		}
-		return max(re, temp);
-	}
+class PrefixFreeSets {
+public:
+	PrefixFreeSets();
+	int maxElements(vector<string>);
 };
+
+PrefixFreeSets::PrefixFreeSets(){}
+
+int PrefixFreeSets::maxElements(vector<string> words){
+	//Sort the list 
+	sort(words.begin(), words.end());
+	int re = 1;
+	int temp = 1;
+	for (int i = 0; i < int( words.size() ) - 1; i++){
+        for (int j = i  + 1; j < int(words.size()); j++){
+            if (!isPrefix(words[i], words[j]) and !isPrefix(words[j], words[j - 1])){
+                temp++;
+            }
+        }
+        re = max(re, temp);
+        temp = 1;
+	}
+	return max(re, temp);
+}
