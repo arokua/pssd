83ffdc088562c8384b3f03656e69df4c7a2cf013
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2019-05-22 01:45:54 +0000

    LotterWItterative

--- week07practice/LotteryTicket.hpp
+++ week07practice/LotteryTicket.hpp
@@ -1,5 +1,6 @@
 #include <string>
-
+#include <iostream>
+#include <algorithm>
 using namespace std;
 
 class LotteryTicket {
@@ -7,44 +8,17 @@ class LotteryTicket {
     string buy(int price, int a, int b, int c, int d){
         string go = "POSSIBLE";
         string nGo = "IM" + go;
-        
-        if (price == a || price == b || price == c || price == d){
-            return go;
+        int arr[4] = {a, b,c, d};
+        sort(arr, arr + 4);
+        if (price < arr[0] or price > 4 * arr[3]) return nGo;
+        for (int i = 0; i < 4; i++){
+            int sum = arr[i];
+            for (int j = i + 1; j < 4; j++){
+                if (sum == price) return go;
+                sum += arr[j];
+            }
+            if (sum == price) return go;            
         }
-        else if (price == a + b){
-            return go;
-        }else if (price == a + c){
-            return go;
-        }else if (price == a + d){
-            return go;
-        }
-        else if (price == b + c ) {
-            return go;
-        }
-        else if (price == b + d){
-            return go;
-        }
-        else if (price == c + d ){
-            return go;
-        }
-        else if (price == a + b + c){
-            return go;
-        }
-        else if (price == a + b + d){
-            return go;
-        }
-        else if (price == b + c + d){
-            return go;
-        }
-	else if ( price == a + c +d){
-	    return go;
-	}
-        else if ( price == a+ b+ c+ d){
-            return go;
-        }
-        
-        else {
-            return nGo;
-        }
+        return nGo;
     }
 };
