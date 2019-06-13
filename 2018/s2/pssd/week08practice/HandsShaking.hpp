b4d0c566bbd08294d8f205ad158bcd4e1dc75ccc
a1733829 <a1733829@2c956862-f5a2-48e2-ae09-16d147905530>
2019-06-13 06:26:00 +0000

    removeRedundant

--- 2018/s2/pssd/week08practice/HandsShaking.hpp
+++ 2018/s2/pssd/week08practice/HandsShaking.hpp
@@ -7,6 +7,7 @@ using namespace std;
 
 class HandsShaking{
     public:
+    //map thenumber of humans to the number of perfect shakes
     map<int, long> holder;
     long countPerfect(int n){
         if (n < 6){
@@ -17,15 +18,15 @@ class HandsShaking{
             holder[n] = 5;
             return 5;
         }else if (holder.find(n) != holder.end()) {
-            
+            // n is recorded
             return holder[n];  
         }else {
             long sum = 0;
             for (int i = 0; i < n; i+=2){
             
                 if (i == 0){
-                    sum += countPerfect(n- i - 2);
-                }else if ( n - i - 2 == 0){
+                    sum += countPerfect(n - 2);
+                }else if ( n - i == 2){
                     sum += countPerfect(i);
                 }else {
                     sum += countPerfect(i) * countPerfect(n - i - 2);
